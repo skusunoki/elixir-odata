9707d6cf847e06c867e66bf40235051b4cd9234f
skusunoki <skusunoki@gmail.com>
2024-03-30 18:15:04 +0900

    test: fix doctest

--- lib/odata.ex
+++ lib/odata.ex
@@ -2,14 +2,14 @@ defmodule OData do
   @moduledoc """
   OData is simple Elixir OData client.
 
-  Usage 1:
+  Usage #1:
   entity
   |> build_query()
   |> set_query_params(params)
   |> build_request(url)
   |> call()
 
-  Usage 2:
+  Usage #2:
   call( entity, url, params )
   """
   @typep call_result :: {:ok, OData.Response.t()} | {:error, any}
@@ -18,11 +18,11 @@ defmodule OData do
   @doc """
   Build a new query to the given entity.
 
-  iex> OData.build_query("People")
-  %OData.Query{service_root: "odata", entity: "People", id: nil, params: %{}}
+      iex> OData.build_query("People")
+      %OData.Query{service_root: "odata", entity: "People", id: nil, params: %{}}
 
-  iex> OData.build_query("People", "service-root")
-  %OData.Query{service_root: "service-root", entity: "People", id: nil, params: %{}}
+      iex> OData.build_query("People", "service-root")
+      %OData.Query{service_root: "service-root", entity: "People", id: nil, params: %{}}
   """
   @spec build_query(String.t()) :: Query.t()
   @spec build_query(String.t(), String.t()) :: Query.t()
@@ -32,14 +32,14 @@ defmodule OData do
   @doc """
   Add new query keyword list of parameters. :top, :skip, :expand, :select, and :filter are acceptable.
 
-  iex> %OData.Query{service_root: "odata", entity: "People", id: nil, params: %{}}
-  iex> |> OData.set_query_params( [ select: "FirstName, LastName"] )
-  %OData.Query{
-  service_root: "odata",
-  entity: "People",
-  id: nil,
-  params: %{select: "FirstName, LastName"}
-  }
+      iex> %OData.Query{service_root: "odata", entity: "People", id: nil, params: %{}}
+      iex> |> OData.set_query_params( [ select: "FirstName, LastName"] )
+      %OData.Query{
+        service_root: "odata",
+        entity: "People",
+        id: nil,
+        params: %{select: "FirstName, LastName"}
+      }
   """
   @spec set_query_params( Query.t, Keyword.t ) :: Query.t
   defdelegate set_query_params(query, params), to: Query, as: :set_params
@@ -47,9 +47,18 @@ defmodule OData do
   @doc """
   Build a new request to the given URL.
 
-  iex> %OData.Query{service_root: "service-root", entity: "People", id: nil, params: %{}}
-  iex> |> OData.build_request( "http://localhost" )
-  %OData.Request{url: "http://localhost", query: %OData.Query{service_root: "service-root", entity: "People", id: nil, params: %{}}}
+      iex> %OData.Query{service_root: "service-root", entity: "People", id: nil, params: %{}}
+      iex> |> OData.build_request( "http://localhost" )
+      %OData.Request{
+        url: "http://localhost",
+        query:
+          %OData.Query{
+                  service_root: "service-root",
+                  entity: "People",
+                  id: nil,
+                  params: %{}
+          }
+      }
   """
   @spec build_request(Query.t, String.t) :: Request.t
   defdelegate build_request(query, url), to: Request, as: :build
